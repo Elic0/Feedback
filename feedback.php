9ce23630f0dfc33b3a88ce1f7424afd290067a02
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2024-02-29 08:03:06 +0100

    .

--- feedback.php
+++ feedback.php
@@ -1,13 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Feedback</title>
-</head>
-<body>
+<?php
 
-<a href="index.html">
-    <img src="images/Feedback-icon.png" alt="Feedback" width="100" height="100">
-</a>
+?>
+<style>
+ .button-container {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+ }
 
-</body>
-</html>
+ .feedback-link {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    opacity: 0.9;
+    transition: opacity 0.3s ease;
+ }
+
+ .feedback-link img {
+    width: 50px;
+    height: auto;
+    transform: rotate(270deg);
+ }
+
+ .feedback-link:hover {
+    opacity: 1;
+ }
+</style>
+
+<div class="button-container">
+ <a href="index.html" class="feedback-link">
+    <img src="../Images/Feedback-icon.png" alt="Feedback">
+ </a>
+</div>
